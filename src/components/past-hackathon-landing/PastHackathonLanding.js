5b3b14ddb1567c0b85e14203476206d7efce3907
hetsuthar028 <hetmewada028@gmail.com>
2021-11-10 13:42:23 +0530

    Past Hackathons section updated

--- src/components/past-hackathon-landing/PastHackathonLanding.js
+++ src/components/past-hackathon-landing/PastHackathonLanding.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useHistory } from 'react-router';
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea, Container, Grid } from "@mui/material";
 import useStyles from "../ui/Style";
-
-const postHackathons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import axios from "axios";
+import './PastHackathonLanding.css';
 
 const PastHackathonLanding = () => {
     const classes = useStyles();
+    const [pastHackathons, setPastHackathons] = useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        axios.get('http://localhost:4400/api/hackathon/get/pastHackathons', {
+            headers: {
+                authorization: localStorage.getItem('session')
+            }
+        })
+            .then((responses) => {
+                if(responses){
+                    console.log("Past Hackathons", responses.data)
+                    setPastHackathons(responses.data.responses.get_past_hackathons_db.pastHackathons);
+                } else {
+
+                }
+            }).catch((err) => {
+                console.log("Error getting past hackathons", err);
+
+            })
+    }, []);
+
+    const handleCardClick = (hackId) => {
+        history.push(`hackathon/view/${hackId}`)
+    }
 
     return (
         
@@ -17,11 +43,11 @@ const PastHackathonLanding = () => {
 
             <h1>Past Hackathons</h1>
 
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }}>
-                {postHackathons.map((hackathon) => (
+            <Grid container>
+                {pastHackathons.map((hackathon) => (
                     <Grid item xs={2} sm={4} md={3} key={hackathon}>
-                        <Card sx={{ maxWidth: 500 }}>
-                            <CardActionArea>
+                        <Card sx={{ maxWidth: 700 }}>
+                            <CardActionArea onClick={() => {handleCardClick(hackathon.id)}}>
                                 <CardMedia
                                     component="img"
                                     height="140"
@@ -32,13 +58,21 @@ const PastHackathonLanding = () => {
 
                                 <CardContent className={classes.cardContent}>
                                     <Typography
+                                        variant="h6"
+                                        fontFamily="Open Sans"
+                                        gutterBottom
+                                    >
+                                        {hackathon.title}
+                                    </Typography>
+
+                                    <Typography
                                         variant="body2"
                                         color="text.secondary"
                                         gutterBottom
+                                        style={{textAlign: "justify", justifyContent: "center"}}
+                                        className="line-clamp module"
                                     >
-                                        World's first Student driven hackathon.
-                                        This hackathon contains various
-                                        technologies.
+                                        {hackathon.description}
                                     </Typography>
                                 </CardContent>
                             </CardActionArea>
